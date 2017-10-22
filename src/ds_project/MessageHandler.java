de2051feee0daad177f9da01969ef8b04a485d5a
Buddhi Ayesha <buddhiayesha.13@cse.mrt.ac.lk>
2017-10-22 21:45:56 +0530

    Add JOIN & SER

---
+++ src/ds_project/MessageHandler.java
@@ -0,0 +1,93 @@
+package ds_project;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ *
+ * @author Buddhi
+ */
+public class MessageHandler {
+
+    public static List<String> message_list;
+    public static List<Neighbour> neighbours_list;
+
+    public MessageHandler() {
+        message_list = new ArrayList<>();
+    }
+
+    /**
+     * @param message
+     *
+     *
+     * length JOIN IP_address port_no
+     * length JOINOK value
+     * length LEAVE IP_address port_no
+     * length LEAVEOK value
+     * length SER IP port file_name hops
+     * length SEROK no_files IP port hops filename1 filename2
+     * length ERROR
+     */
+    public void handle(String message) {
+
+        try {
+            String[] mes = message.split(" ");
+
+            switch (mes[1]) {
+                case "SER":
+                    String[] newMes = null;
+                    Arrays.copyOfRange(newMes, 0, (mes.length - 2));
+                    String uniqMes = String.join("", newMes);
+                    addMessage(uniqMes);
+                    break;
+                case "JOIN":
+                    if (!addMessage(message)) {
+                        if(addNeighbours(new Neighbour(mes[2], mes[3]))){
+                            
+                        }
+                    }
+                    break;
+                case "JOINOK":
+                    addMessage(message);
+                    break;
+                case "LEAVE":
+                    break;
+                case "LEAVEOK":
+                    break;
+
+                case "SEROK":
+                    break;
+                case "ERROR":
+                    break;
+
+                default:
+                    System.err.println("Error: " + message);
+                    break;
+            }
+
+        } catch (Exception e) {
+            System.err.println(e);
+        }
+
+    }
+
+    public boolean addNeighbours(Neighbour neb) {
+        if (!neighbours_list.stream().noneMatch((tem) -> (tem == neb))) {
+            return false;
+        }
+        
+        neighbours_list.add(neb);
+        return true;
+    }
+
+    public synchronized boolean addMessage(String message) {
+        if (message_list.stream().anyMatch((str) -> (str.equals(message)))) {
+            return true;
+        } else {
+            message_list.add(message);
+            return false;
+        }
+    }
+
+}
