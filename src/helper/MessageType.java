952f5a617b144f4a0f45cf47a4fd96cb5b01ccd9
Buddhi Ayesha <buddhiayesha.13@cse.mrt.ac.lk>
2017-10-23 20:12:30 +0530

    Change help to helper

---
+++ src/helper/MessageType.java
@@ -0,0 +1,9 @@
+package helper;
+
+/**
+ *
+ * @author Hareen Udayanath
+ */
+public enum MessageType {
+     REG, UNREG, JOIN, JOINOK,INQUIRE,INQUIREOK, LEAVE,LEAVEOK, SER, SEROK, REG_SUCCESS, REG_FAILURE, FILES
+}
