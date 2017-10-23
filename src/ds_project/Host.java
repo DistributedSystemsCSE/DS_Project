acd7b1b2bc6bbd0f289c7b864f493d95ae0c8aeb
HareenUdayanath <hareenudayanath@gmail.com>
2017-10-23 21:26:08 +0530

    Host class

---
+++ src/ds_project/Host.java
@@ -0,0 +1,17 @@
+package ds_project;
+
+/**
+ *
+ * @author Hareen Udayanath
+ */
+public class Host {
+    private String ip;
+    private int port;
+    
+    public Host(){
+    }
+    public Host(String ip,int port){
+        this.ip = ip;
+        this.port = port;
+    }
+}
