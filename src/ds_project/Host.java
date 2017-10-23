9fa4b28118b1f197edea9cf3ebd9df058f66d88c
HareenUdayanath <hareenudayanath@gmail.com>
2017-10-23 21:29:03 +0530

    Code refactoring

--- src/ds_project/Host.java
+++ src/ds_project/Host.java
@@ -14,4 +14,33 @@ public class Host {
         this.ip = ip;
         this.port = port;
     }
+
+    /**
+     * @return the ip
+     */
+    public String getIp() {
+        return ip;
+    }
+
+    /**
+     * @param ip the ip to set
+     */
+    public void setIp(String ip) {
+        this.ip = ip;
+    }
+
+    /**
+     * @return the port
+     */
+    public int getPort() {
+        return port;
+    }
+
+    /**
+     * @param port the port to set
+     */
+    public void setPort(int port) {
+        this.port = port;
+    }
+    
 }
